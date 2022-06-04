106e9c339acbe537845f94db4adeee923397b12b
Arseniy <verseny@bk.ru>
2022-06-04 11:36:08 +0300

    Add

---
+++ ANVergizov_7/fourth.py
@@ -0,0 +1,15 @@
+import os
+
+folder = '.'
+dict = {}
+for root, dirs, files in os.walk(folder):
+    for file in files:
+        size = os.stat(os.path.join(root,file)).st_size
+        key = 10 ** len(str(size))
+        if key in dict:
+            dict[key] += 1
+        else:
+            dict[key] = 1
+
+for key, value in sorted(dict.items()):
+    print(key, value)
